3f176e792c5e343892a50a4df89e78593f78ddda
Philipp Liegl <philipp.liegl@ecosio.com>
2015-05-27 22:02:04 +0200

    ADDED text filters for EDIFACT files

---
+++ TextFilters/expandEDIFACT.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#Install GNU Command line tools in order to make this work, as the MAC OS 'sed' is unable
+#to properly deal with '\n' in the replacement section
+#See the following link for further details on how to install GNU Command line tools
+#https://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in-mac-os-x/
+
+#Replace all ' with '\n
+gsed 's/'\''/'\''\n/g'
